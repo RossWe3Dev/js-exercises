f41f550087b7eb74daefc2981da3b389741261d4
Ross we3dev <168005603+RossWe3Dev@users.noreply.github.com>
2024-05-28 17:42:46 +0200

    Complete Exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,33 +1,12 @@
-const removeFromArray = function (arr, ...toRemove) {
+const removeFromArray = function (arr, ...argsToRemove) {
 
-    let index1 = [];
-    for (const argument of toRemove) {
-        index1 = arr.indexOf(argument);
-        arr.splice(index1, 1);
+    for (const argument of argsToRemove) {
+        arr = arr.filter((arrElement) => arrElement !== argument);
     }
     return arr;
 
-    /* const index = arr.indexOf(toRemove);     //! same arr
-    arr.splice(index, 1);
-    return arr; */
+}
 
-    /* const splicedArr = arr.toSpliced(index, 1);      //! new arr with toSpliced()
-    return splicedArr; */
-};
 
 // Do not edit below this line
 module.exports = removeFromArray;
-
-
-/** Suggestion from Reddit Post
- * 
- * const indexOfAll = (str, target) => Array.from(str)
-  .map((char, i) => char === target ? i : -1)
-  .filter(i => i !== -1);
-
-indexOfAll('hello', 'l');  // [2, 3]
-
-For loops are rarely the best way to process data. Occasionally I’ll use a for...of if I’m off down some edge case,
-but a combination of array methods (like Array.from, map, and filter here) is usually more readable, less error prone,
-and easier to both read and write.
- */
