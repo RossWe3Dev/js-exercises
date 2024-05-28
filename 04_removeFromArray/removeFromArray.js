33ddb3156eec0c8fcf61988e6eaf98a4d328a374
Ross we3dev <168005603+RossWe3Dev@users.noreply.github.com>
2024-05-28 12:06:24 +0200

    Complete Exercise 3, pass first test of Exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, toRemove) {
+    const index = arr.indexOf(toRemove);
+    arr.splice(index, 1);
+    return arr;
+
+    /* const splicedArr = arr.toSpliced(index, 1);
+    return splicedArr; */
 };
 
 // Do not edit below this line
