f031f78ccd55817555a615ff20bda7c1afeecdf5
Ross we3dev <168005603+RossWe3Dev@users.noreply.github.com>
2024-05-28 12:08:28 +0200

    Complete Exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+    const lettersArr = string.split("");
+    lettersArr.reverse();
+    let reverseString;
+    reverseString = lettersArr.join("");
+    return reverseString;
 };
 
 // Do not edit below this line
