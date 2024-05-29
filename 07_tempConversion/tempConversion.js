f473623cf92b28a9e57bb7d69ed39a664da25c3b
Ross we3dev <168005603+RossWe3Dev@users.noreply.github.com>
2024-05-29 12:46:01 +0200

    Complete Exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheit) {
+  return round(((fahrenheit - 32) * 5 / 9), 1);    //* precision set to show 1 decimal
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsius) {
+  return round((celsius / 5 * 9 + 32), 1);      //* precision set to show 1 decimal
 };
+
+// function that rounds a number to the required precision   //* As explained @ https://stackoverflow.com/q/7342957/5433628
+function round(value, precision) {
+  let multiplier = Math.pow(10, precision || 0);
+  return Math.round(value * multiplier) / multiplier;
+}
 
 // Do not edit below this line
 module.exports = {
