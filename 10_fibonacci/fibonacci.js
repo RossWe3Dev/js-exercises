351bb798039f078519caa52f5c8ab904b05272c8
Ross we3dev <168005603+RossWe3Dev@users.noreply.github.com>
2024-06-11 21:33:32 +0200

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function () {
-
+const fibonacci = function (x) {
+    x = parseInt(x);
+    if (x < 0) return "OOPS";
+    let fibArr = [0, 1];
+    for (i = 2; i <= x; i++) {
+        fibArr.push(fibArr[i - 2] + fibArr[i - 1]);
+    };
+    return fibArr[x];
 };
 
 // Do not edit below this line
