5c6a684a228d7be0662b780f3eef3174d0a9493b
Ross we3dev <168005603+RossWe3Dev@users.noreply.github.com>
2024-06-12 11:58:51 +0200

    Solve exercise 12
    The issue was that the function was directly returning the name property
    of the first (index = 0) person object of the array people while the
    test was trying to access the name property as well.
    Solved the issue by returning the entire object at the first (index=0)
    position.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -3,7 +3,7 @@ getAge = (x) => (x.yearOfDeath || currentYear) - x.yearOfBirth;
 
 const findTheOldest = function (arr) {
     arr.sort((person, nextPerson) => getAge(nextPerson) - getAge(person));
-    return arr[0].name;
+    return arr[0];
 };
 
 // Do not edit below this line
